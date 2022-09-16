33628f58f0e7cbbc997df911e5b7bb1777d0cc74
Nabanita Sarkar <nabanita.sarkar1418@gmail.com>
2022-09-16 13:16:29 +0530

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "./",
+  plugins: [react()],
+  server: {
+    port: 5000,
+  },
+  resolve: {
+    alias: {
+      components: path.resolve("src/components"),
+      pages: path.resolve("src/pages"),
+      styles: path.resolve("src/styles"),
+    },
+  },
+});
